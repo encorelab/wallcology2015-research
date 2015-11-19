f7156b239c09fe8e6d65950fd2cbdb6354c6e9e3
Stian Håklev <shaklev@gmail.com>
2015-11-18 20:48:15 -0500

    Functional prototype

--- wall-graph/wallgraph.js
+++ wall-graph/wallgraph.js
@@ -11,18 +11,58 @@ function getUrlParams() {
 }
 
 params = getUrlParams()
-console.log(params)
-nutella = NUTELLA.init("ltg.evl.uic.edu", "wallcology", params.run, "wallcology_admin", 
-                       function(e) { console.log(e) }, {secure: true})
-console.log("Nutella connected", nutella)
-message = {habitat: 0 + params.habitat, points: 100, from: new Date("24 October, 2015"), 
+nutella = NUTELLA.init("ltg.evl.uic.edu", "wallcology", 
+  params.run, "wallcology_admin", 
+  function(e) { 
+    console.log("Nutella connecting", e) }, {secure: true})
+
+message = {habitat: parseInt(params.habitat), points: 100, 
+  from: new Date("24 October, 2015").getTime(), 
   to: Date.now()}
+
+window.data = []
 _.chain(_.range(0,15)).each(function(x) {
   msg = _.extend(message, {species: x})
+  var pop_array;
   nutella.net.request("population_history", msg, function(e) { 
-    var pop_array = _.chain(e).map(function(x) { 
-      return(x.population) }).value()
-    console.log(pop_array)
+    window.data.push(e)
   })
 })
 
+function transformData(data) {
+  return [data.timestamp, data.population]
+}
+
+function transformSeries(series) {
+  return {data: _.map(series, transformData)}
+}
+window.olddata = {}
+function graph() {
+  console.log("updating graph")
+  var graphobj = {
+        chart: {
+            type: 'line'
+        },
+        title: {
+            text: 'Historical population graph'
+        },
+        xAxis: {
+          type: 'datetime'
+            // categories: ['Apples', 'Bananas', 'Oranges']
+        },
+        yAxis: {
+            // title: {
+            //     text: 'Fruit eaten'
+            // }
+        },
+        series: _.map(window.data, transformSeries)
+    }
+    window.graphobj = graphobj
+    if(!_.isEqual(window.olddata, window.data)) { 
+      $('#container').highcharts(graphobj);
+      window.olddata = _.clone(window.data);
+    }
+    setTimeout(function(){ graph()}, 30000);
+}
+
+setTimeout(function(){ graph()}, 4000);
